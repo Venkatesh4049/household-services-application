b6d7c6a965295f1e417bed4c9011a05e87593ac9
Venkatesh A M <appiyavenkatesh@gmail.com>
2025-01-18 11:11:24 +0530

    Delete MAD 1 HSA/code directory

--- MAD 1 HSA/code/deletedb.py
+++
@@ -1,8 +0,0 @@
-from app import app, db  
-
-
-with app.app_context():
-    for table in reversed(db.metadata.sorted_tables):
-        db.session.execute(table.delete())
-    db.session.commit()
-    print("All data has been deleted from the database.")
